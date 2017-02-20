667393ad1bbfe0c5ccfdca4aadeda103631e5ecf
Kimilhee <iamarej@gmail.com>
2017-02-20 14:38:03 +0900

    templates 위치 변경.

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -4,19 +4,19 @@ export default function routing($stateProvider, $urlRouterProvider) {
     $stateProvider
     .state('login', {
         url: '/login',
-        templateUrl: 'templates/login.html',
+        template: require('./templates/login.html'),
         controller: 'LoginCtrl'
     })
     .state('main', {
         url: '/main',
-        templateUrl: 'templates/main.html',
+        template: require('./templates/main.html'),
         controller: 'MainCtrl'
     })
-    // .state('secure', {
-    //     url: '/secure',
-    //     templateUrl: 'templates/secure.html',
-    //     controller: 'SecureCtrl'
-    // });
+    .state('secure', {
+        url: '/secure',
+        template: require('./templates/secure.html'),
+        controller: 'SecureCtrl'
+    });
     $urlRouterProvider.otherwise('/');
 }
 console.log('app.config');
